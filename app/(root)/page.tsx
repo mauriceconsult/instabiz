f709887cadcfe72184441897645485eda8af2ceb
Maurice Consultant <mainna62@outlook.com>
2026-02-17 12:04:00 +0300

    feat: add store creation modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+useEffect(() => {
+  if (!isOpen) {
+    onOpen();
+  }
+}, [isOpen, onOpen]);
   return (
     <div className="p-4">
-      <Button size={"default"}>Click me</Button>
+      <h1 className="text-2xl font-bold mb-4">Setup Page</h1>
     </div>   
   );
 }
+export default SetupPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "InstaBiz - Multi-Vendor E-commerce Platform",
@@ -26,9 +17,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
+          <ModalProvider />
           {children}
         </body>
       </html>
