17f56bc0dd4d9e4fd738d2a2071638b46c4a0962
Maurice Consultant <mainna62@outlook.com>
2026-02-28 15:26:11 +0300

    Fixed category bug

--- app/api/[shopId]/categories/[categoryId]/route.ts
+++ app/api/[shopId]/categories/[categoryId]/route.ts
@@ -15,6 +15,9 @@ export async function GET(
       where: {
         id: (await params).categoryId,
       },
+      include: {
+        billboard: true,
+      }
     });
 
     return NextResponse.json(category);
